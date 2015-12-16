d5f5cbb2e5c7d53f974c3a840a3c91d0d7d7f961
Somy Andriyanto <somyandriyanto@gmail.com>
2015-12-17 01:44:40 +0700

    Fix test bootstraping

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-//var_dump(dirname(__DIR__) . '/vendor/autoload.php');
 
 if (!require dirname(__DIR__) . '/vendor/autoload.php') {
     die('You must set up the project dependencies, run the following commands:
