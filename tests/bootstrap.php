2b115503590d5a9b6f51db9174df99b6b3907ff9
Somy Andriyanto <somyandriyanto@gmail.com>
2015-12-17 01:41:17 +0700

    Fix test bootstraping

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
-if (!@require __DIR__ . '/../vendor/autoload.php') {
+//var_dump(dirname(__DIR__) . '/vendor/autoload.php');
+
+if (!require dirname(__DIR__) . '/vendor/autoload.php') {
     die('You must set up the project dependencies, run the following commands:
         wget http://getcomposer.org/composer.phar
         php composer.phar install');
